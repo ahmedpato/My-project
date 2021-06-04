d4bac6f9bb3b4b3168c4d9f486d7b6e46295cc9d
Programmer <67549087+ahmedpato@users.noreply.github.com>
2021-06-04 19:15:33 +0200

    Add files via upload

---
+++ HomeWork1.cpp
@@ -0,0 +1,40 @@
+// By : Ahmed Mohammed Ali / 20184020
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{  
+    vector<int> numbers;
+    
+    cout<<"Enter 5 Numbers :"<<endl; 
+    int input = 0;
+    for(int i=0 ; i<5 ; i++){
+        cin>>input;
+        numbers.push_back(input);
+    }
+    // print Vector 
+    cout<<"The Vector Is :"<<endl;
+    for(int i=0 ; i<numbers.size(); i++){
+        cout<<numbers[i]<<endl;
+    }
+    int sub ; 
+    sub = numbers[0];
+    for(int i=1 ; i<numbers.size(); i++){
+        sub -= numbers[i];
+    }
+    cout<<"The Subtraction of Vector Is :"<<sub<<endl;
+    cout<<"The Summation Of Vector Is :"<< accumulate(numbers.begin(),numbers.end() , 0)<<endl;
+    cout<<"The Muliplication Of Vector :"<< accumulate(numbers.begin(),numbers.end(),1,multiplies<int>())<<endl;
+
+    float div;
+    div = numbers[0];
+    for(int i=1 ; i<numbers.size() ; i++){
+        div /= numbers[i];
+    }
+    cout<<"The Division Of Vector Is :"<<div<<endl;
+    cout<<"The Min Number In Vector :"<< *min_element(numbers.begin(),numbers.end())<<endl;
+    cout<<"The Max Number In Vector :"<< *max_element(numbers.begin(),numbers.end())<<endl;  
+}
