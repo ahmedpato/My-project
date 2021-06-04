b00a0f279c53f829aab7bc1ccbb501991a884259
Programmer <67549087+ahmedpato@users.noreply.github.com>
2021-06-04 19:24:03 +0200

    Update HomeWoek5.cpp

--- HomeWoek5.cpp
+++ HomeWoek5.cpp
@@ -2,26 +2,16 @@
 
 /*
 Question : 
-Using stack, write a
-program that represents a web browser which will show the following menu to the
-user:
 
+Using stack, write a program that represents a web browser which will show the following menu to the user:
 
-1-     
-Enter website (the user
-will enter a website i.e. www.google.com
-and the website will be saved in the web browser history stack).
+1-Enter website (the user will enter a website i.e. www.google.com and the website will be saved in the web browser history stack).
 
+2-Show all history.
 
-2-     
-Show all history.
+3-Delete all history.
 
-
-3-     
-Delete all history.
-
-
-4-      Exit.
+4-Exit.
 
 
 */
@@ -61,15 +51,17 @@ int main()
     int choice;
     while(true)
     {
-        cout<<"Add"<<endl;
-        cout<<"Show"<<endl;
-        cout<<"Delete"<<endl;
+        cout<<"1_ Add ."<<endl;
+        cout<<"2_ Show ."<<endl;
+        cout<<"3_ Delete ."<<endl;
+        cout<<"4_ Exit ."<<endl;
         cout<<"Enter Your Choice :"; cin>>choice;
         switch(choice)
         {
         case 1 : Insert_Web(web); break;
         case 2 : Show_All(web); break;
         case 3 : Delete_All(web); break;
+        case 4 : exit(0); break;
         
         default:
             break;
